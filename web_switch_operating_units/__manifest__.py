32421347fc9acff4da65907c63e38bc4439d68f2
Vishnu Vanneri <vanneri.odoodev@gmail.com>
2020-11-24 17:33:32 +0530

    [ADD] web_switch_operating_units: For Switching Multiple OP's From Front View

---
+++ web_switch_operating_units/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Easy Operating Unit Switching",
+
+    "summary": """
+        User With Multiple Operating Unit (OU) Can Easily Switch From Menu""",
+    "version": "12.0.1.0.0",
+    "author": "VaNnErI",
+    "category": "Hidden",
+    "depends": ["web", "operating_unit"],
+    "license": "LGPL-3",
+    "data": [
+        "views/web_switch_operating_units.xml",
+    ],
+    "qweb": [
+        "static/src/xml/switch_op.xml",
+    ],
+}
